26fde19990a4ad6b9a18928ede3e1641482d0db1
Mike Lynch <nycynik@gmail.com>
2021-03-09 16:55:46 -0800

    LCD showing some information

---
+++ arduino/lcd/lcd.ino
@@ -0,0 +1,20 @@
+#include <LiquidCrystal.h>
+
+const int rs=12, en=11, d4=5, d5=4, d6=3, d7=2;
+LiquidCrystal lcd(rs, en, d4, d5,d6,d7);
+
+ void setup() {
+  // put your setup code here, to run once:
+  lcd.begin(16, 2);
+  
+   
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  lcd.setCursor(0,0);
+  lcd.print("Hello Mike");
+  delay(500); 
+  lcd.setCursor(0,1);
+  lcd.print("3/9/2021"); 
+}
