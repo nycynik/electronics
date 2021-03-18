59965051d2e0d31db04bce626cf8217c1f7e85bf
Mike Lynch <nycynik@gmail.com>
2021-03-17 21:53:46 -0700

    Seven Segment LED counting from 0 to 9

--- arduino/sevenseg/sevenseg.ino
+++ arduino/sevenseg/sevenseg.ino
@@ -1,12 +1,30 @@
 int COUNTER = 0;
 const int ledPinsAF[] = {2,3,4,5,6,7,8};
+const int dataPin = 8;
+const int latchPin = 10;
+const int clockPin = 11;
+
+
+// the binary nmbers 0-9 in order
+int dataArray[10] = {
+  0B00000011, // 0
+  0B10011111, // 1
+  0B00100101, // 2
+  0B00001101, // 3
+  0B10011001, // 4
+  0B01001001, // 5
+  0B01000001, // 6
+  0B00011111, // 7
+  0B00000001, // 8
+  0B00001001  // 9
+};
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  for (int x=0; x<7; x++) {
-    pinMode(ledPinsAF[x], OUTPUT);
-  }
+  pinMode(latchPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);
   
 }
 
@@ -19,13 +37,14 @@ void turnOffAllLEDS() {
 void loop() {
   // put your main code here, to run repeatedly:
 
-  turnOffAllLEDS();
-  // now turn on one
-  digitalWrite(ledPinsAF[COUNTER], LOW);
-  delay(700);
+  
+  for (int x=0; x<10; x++) {
+    // shiftOut(dataPin, ClickPin, BitOrder, daata);
+    digitalWrite(latchPin, LOW);
+    shiftOut(dataPin, clockPin, MSBFIRST, dataArray[x]);
+    digitalWrite(latchPin, HIGH);
 
-  COUNTER++;
-  if (COUNTER>7) {
-    COUNTER = 0;
+    delay(300);
   }
+  
 }
